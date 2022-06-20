0847f0e7134e5f92d4296ca392de44d32b5fc13e
majura mazonge <seif0ghalib@gmail.com>
2022-06-20 15:48:22 -0400

    Last version with callback 06/20/22

--- app/src/main/java/com/example/customsnackbar/MainActivity.kt
+++ app/src/main/java/com/example/customsnackbar/MainActivity.kt
@@ -4,17 +4,42 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.customsnackbar.chef.ChefSnackbar
+import com.example.customsnackbar.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_INDEFINITE
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        findViewById<View>(R.id.button_chef).setOnClickListener {
-            ChefSnackbar
-                .make(it)
-                .show()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonChef.setOnClickListener {
+            val chefSnackbar = ChefSnackbar.make(it)
+            chefSnackbar.addCallback(snackBar)
+                .duration = LENGTH_INDEFINITE
+
+            chefSnackbar.show()
         }
     }
+
+   private val snackBar = object : BaseTransientBottomBar.BaseCallback<ChefSnackbar>() {
+       override fun onDismissed(transientBottomBar: ChefSnackbar?, event: Int) {
+
+           binding.textViewChef.text = event.toString()
+       }
+
+       override fun onShown(transientBottomBar: ChefSnackbar?) {
+
+           transientBottomBar?.view?.setOnClickListener {
+               transientBottomBar.dismiss()
+           }
+
+           binding.textViewChef.text = "NIMEIONAAA"
+       }
+   }
 }
